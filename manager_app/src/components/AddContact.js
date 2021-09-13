397c3fb120e487e5019467bbda3e1d0554ca7ba9
FrazierMark <60164353+FrazierMark@users.noreply.github.com>
2021-09-12 17:10:07 -0700

    Added onChange and onSubmite to AddContact

--- manager_app/src/components/AddContact.js
+++ manager_app/src/components/AddContact.js
@@ -1,18 +1,43 @@
-import React from "react"
+import { applyProps } from "@react-three/fiber";
+import React, {useState} from "react"
 
-const AddContact = () => {
+const AddContact = (props) => {
+
+    const [values, setValues] = useState({
+        name: '',
+        email: '',
+    })
+
+    const handleChange = name => e => {
+        setValues({ ...values, [name]: e.target.value });
+    };
+   
+
+    const handleSubmit = (e) => {
+        e.preventDefault();
+        if(values.name === "" && values.email=== "") {
+            alert("All fields are mandatory!")
+            return
+        }
+            props.addContactHandler(values)
+        console.log(values)
+
+    }
+
+
+
     return (
         <div className="ui main">
             <h1>Add Contact</h1>
             <h1>Add Contact</h1>
-            <form className="ui form"> 
+            <form className="ui form" onSubmit={handleSubmit}> 
                 <div className="field">
                     <label>Name</label>
-                    <input type="text" name="name" placeholder="Name"/>
+                    <input type="text" name="name" placeholder="Name" onChange={handleChange('name')}/>
                 </div>
                 <div className="field">
                     <label>Name</label>
-                    <input type="text" name="name" placeholder="Name"/>
+                    <input type="text" name="email" placeholder="email" onChange={handleChange('email')}/>
                 </div>
                 <button className="ui button blue">Add</button>
             </form>
--- manager_app/src/components/App.js
+++ manager_app/src/components/App.js
@@ -1,26 +1,19 @@
-import React from "react"
+import React, {useState} from "react"
 import './App.css';
 import Header from "./Header";
 import AddContact from "./AddContact";
 import ContactList from "./ContactList";
 
 const App = () => {
-  const contacts = [
-    {
-      id: "1",
-      name: "Phil",
-      email: "philsander@gmail.com",
-    },
-    {
-      id: "2",
-      name: "Jeremy",
-      email: "Jbear@gmail.com"
-    }
-  ];
+  const [contacts, setContacts] = useState([])
+
+  const addContactHandler = (contact) => {
+      console.log(contact)
+  }
   return (
     <div className="ui container" >
       <Header />
-      <AddContact />
+      <AddContact addContactHandler={addContactHandler} />
       <ContactList contacts={contacts} />
     </div>
   );
