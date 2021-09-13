30267d06be8df99a7d96f6121d4c2bdef4c936b1
FrazierMark <60164353+FrazierMark@users.noreply.github.com>
2021-09-13 12:01:42 -0700

    Created local storage for contacts.

--- manager_app/src/components/App.js
+++ manager_app/src/components/App.js
@@ -1,15 +1,28 @@
-import React, {useState} from "react"
+import React, {useState, useEffect} from "react"
 import './App.css';
 import Header from "./Header";
 import AddContact from "./AddContact";
 import ContactList from "./ContactList";
 
 const App = () => {
+  const LOCAL_STORAGE_KEY = "contacts";
   const [contacts, setContacts] = useState([])
 
   const addContactHandler = (contact) => {
       console.log(contact)
+      setContacts([...contacts, contact])
   }
+  // Grabs any contacts saved in local storage AND sets them in state
+  useEffect(() => {
+    const retrieveContacts = JSON.parse(localStorage.getItem(LOCAL_STORAGE_KEY));
+    if(retrieveContacts) setContacts(retrieveContacts)
+  }, []);
+
+  // Takes contact submission and saves it in local storage
+  useEffect(() => {
+    localStorage.setItem(LOCAL_STORAGE_KEY, JSON.stringify(contacts));
+  }, [contacts]);
+
   return (
     <div className="ui container" >
       <Header />
