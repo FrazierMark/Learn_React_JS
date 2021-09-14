980c2560d315145f546bab8bc13268d46edecd7a
FrazierMark <60164353+FrazierMark@users.noreply.github.com>
2021-09-14 10:46:25 -0700

    Create GET and delete requests.

--- manager_app/client/src/components/App.js
+++ manager_app/client/src/components/App.js
@@ -4,7 +4,7 @@ import './App.css';
 import Header from "./Header";
 import AddContact from "./AddContact";
 import ContactList from "./ContactList";
-import { uuid } from "uuidv4";
+import { uuid, v4 } from "uuidv4";
 import ContactDetail from "./ContactDetail";
 import api from "../api/contacts"
 
@@ -13,16 +13,33 @@ const App = () => {
   const LOCAL_STORAGE_KEY = "contacts";
   const [contacts, setContacts] = useState([])
 
-  const addContactHandler = (contact) => {
+  // RetrieveCOntacts
+  const retrieveContacts = async () => {
+    const response = await api.get("/contacts");
+    return response.data;
+  }
+
+  const addContactHandler = async (contact) => {
       console.log(contact)
-      setContacts([...contacts, {id: uuid(), ...contact }])
+
+      // Save new contact to json db
+      const request = {
+        id: v4(),
+        ...contact
+      }
+
+      // save contact/response to state
+      const response = await api.post("/contacts", request)
+
+      setContacts([...contacts, response.data])
   }
 
     // Overwrites existing contactList, without the deleted contact
     // creates new contact list,
     // filters out contact by id (the contact to delete)
     // sets new state with new contactList minus the deleted contact
-  const removeContactHandler = (id) => {
+  const removeContactHandler = async (id) => {
+    await api.delete(`/contacts/${id}`);
     const newContactList = contacts.filter((contact) => {
       return contact.id !== id;
     })
@@ -34,11 +51,17 @@ const App = () => {
   useEffect(() => {
     // const retrieveContacts = JSON.parse(localStorage.getItem(LOCAL_STORAGE_KEY));
     // if(retrieveContacts) setContacts(retrieveContacts)
+
+    const getAllContacts = async () => {
+      const allContacts = await retrieveContacts();
+      if(allContacts) setContacts(allContacts);
+    };
+    getAllContacts();
   }, []);
 
   // Takes contact submission and saves it in local storage
   useEffect(() => {
-    localStorage.setItem(LOCAL_STORAGE_KEY, JSON.stringify(contacts));
+    // localStorage.setItem(LOCAL_STORAGE_KEY, JSON.stringify(contacts));
   }, [contacts]);
 
   return (
