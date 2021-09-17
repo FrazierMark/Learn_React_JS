4c34908abfce81fd2df609689e4aaddf6246b0a4
FrazierMark <60164353+FrazierMark@users.noreply.github.com>
2021-09-17 16:47:50 -0700

    Mapping poster and images to Row.

--- netflix_clone_app/src/components/Row.js
+++ netflix_clone_app/src/components/Row.js
@@ -1,23 +1,29 @@
 import React, { useState, useEffect } from "react";
 import axios from '../axios'
 
-const Row = ({ title }) => {
+const Row = ({ title, fetchURL }) => {
     const [movies, setMovies] = useState([]);
 
     useEffect(() => {
             
         async function fetchData() {
             const request = await axios.get(fetchURL);
-
+            setMovies(request.data.results);
+            return request;
         }
-
-    }, []);
+        fetchData()
+    }, [fetchURL]);
 
 
     return (
-        <div>
+        <div className="row">
             <h2>{title}</h2>
 
+            <div className="row__posters">
+                {movies.map(movie => (
+                    <img src={movie.poster_path} alt={movie.name} /> 
+                ))}
+            </div>
 
         </div>
     )
--- netflix_clone_app/src/request.js
+++ netflix_clone_app/src/request.js
@@ -3,14 +3,14 @@
 const API_KEY = "51f4e2b0cc83cdcfd78323b52fd4884c";
 
 const requests = {
-    fetchTrending: '/trending/all/week?api_key=${API_KEY}&language=en-US',
-    fetchNetflixOriginals: '/discover/tv?api_key=${API_KEY}&with_networks=123',
-    fetchTopRated: '/movie/top_rated?api_key=${API_KEY}&language=en-US',
-    fetchActionMovies: '/discover/movie?api_key=${API_KEY}&with_genres=28',
-    fetchComedyMovies: '/discover/movie?api_key=${API_KEY}&with_genres=35',
-    fetchHorrorMovies: '/discover/movie?api_key=${API_KEY}&with_genres=27',
-    fetchRomanceMovies: '/discover/movie?api_key=${API_KEY}&with_genres=10749',
-    fetchDocumentaries: '/discover/movie?api_key=${API_KEY}&with_genres=99',
+    fetchTrending: `/trending/all/week?api_key=${API_KEY}&language=en-US`,
+    fetchNetflixOriginals: `/discover/tv?api_key=${API_KEY}&with_networks=123`,
+    fetchTopRated: `/movie/top_rated?api_key=${API_KEY}&language=en-US`,
+    fetchActionMovies: `/discover/movie?api_key=${API_KEY}&with_genres=28`,
+    fetchComedyMovies: `/discover/movie?api_key=${API_KEY}&with_genres=35`,
+    fetchHorrorMovies: `/discover/movie?api_key=${API_KEY}&with_genres=27`,
+    fetchRomanceMovies: `/discover/movie?api_key=${API_KEY}&with_genres=10749`,
+    fetchDocumentaries: `/discover/movie?api_key=${API_KEY}&with_genres=99`,
 
 }
 
