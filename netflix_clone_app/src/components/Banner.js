2164f510afbc7ee81a93cf7cf8f80bf1a56de472
FrazierMark <60164353+FrazierMark@users.noreply.github.com>
2021-09-19 13:48:58 -0700

    Banner styling.

--- netflix_clone_app/src/components/Banner.js
+++ netflix_clone_app/src/components/Banner.js
@@ -1,6 +1,7 @@
 import React, { useState, useEffect } from "react";
 import axios from '../axios';
 import requests from '../requests';
+import './Banner.css'
 
 const Banner = () => {
     const [movie, setMovie] = useState([]);
@@ -20,6 +21,11 @@ const Banner = () => {
         fetchData();
     }, []);
 
+    function truncate(str, n) {
+        return str?.length > n ? str.substr(0, n - 1) + "..." : str;
+    };
+
+
     return (
         <header className="banner"
             style={{
@@ -31,13 +37,15 @@ const Banner = () => {
 
 
             <div className="banner__contents">
-                <h1>
+                <h1 className="banner__title">
                     {movie?.title || movie?.name || movie?.original_name}
                 </h1>
                 <div className="banner__buttons">
                     <button className="banner__button">Play</button>
                     <button className="banner__button">My List</button>
                 </div>
+
+                <h1 classNmae="banner__description">{truncate(movie?.overview, 150)}</h1>
 
             </div>
         </header>
