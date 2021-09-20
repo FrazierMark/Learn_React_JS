39fec44c4f612a94b7f7c30236ba4a84dcf89156
FrazierMark <60164353+FrazierMark@users.noreply.github.com>
2021-09-20 13:46:50 -0700

    Deploying to Firbase.

--- netflix_clone_app/src/App.js
+++ netflix_clone_app/src/App.js
@@ -20,7 +20,7 @@ function App() {
       <Row title="Top Rated" fetchURL={requests.fetchTopRated} />
       <Row title="Action Movies" fetchURL={requests.fetchActionMovies} />
       <Row title="Comedy Movies" fetchURL={requests.fetchComedyMovies} />
-      <Row title="Horror Movies" fetchURL={requests.fetchComedyMovies} />
+      <Row title="Horror Movies" fetchURL={requests.fetchHorrorMovies} />
       <Row title="Romance Movies" fetchURL={requests.fetchRomanceMovies} />
       <Row title="Documentaries" fetchURL={requests.fetchDocumentaries} />
     </div>
