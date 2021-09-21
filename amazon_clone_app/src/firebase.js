e61603f0d4c3452e31843f87e08cc0d88b44a1bf
FrazierMark <60164353+FrazierMark@users.noreply.github.com>
2021-09-21 13:28:08 -0700

    Starting Amazon Clone App.

---
+++ amazon_clone_app/src/firebase.js
@@ -0,0 +1,10 @@
+// For Firebase JS SDK v7.20.0 and later, measurementId is optional
+const firebaseConfig = {
+    apiKey: "AIzaSyALOTDLc9qY4TGlgrHHMHf-tLTSnjefsk8",
+    authDomain: "clone-app-da065.firebaseapp.com",
+    projectId: "clone-app-da065",
+    storageBucket: "clone-app-da065.appspot.com",
+    messagingSenderId: "331855031792",
+    appId: "1:331855031792:web:a65b74c15808e8cbd70135",
+    measurementId: "G-DSD901Z89L"
+  };
