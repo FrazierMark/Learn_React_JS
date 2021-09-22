cc1faefbf64dda7155195c267ce72f2ab147a243
FrazierMark <60164353+FrazierMark@users.noreply.github.com>
2021-09-21 17:27:50 -0700

    Building Header.

--- amazon_clone_app/src/App.js
+++ amazon_clone_app/src/App.js
@@ -1,12 +1,14 @@
 import React from "react"
 import './App.css';
+import Header from "./components/Header";
+
 
 function App() {
   return (
     <div className="app">
-      <h1>Hello World</h1>
       {/* {Header}
       {Home} */}
+      <Header />
 
     </div>
   );
