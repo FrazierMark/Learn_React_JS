5acbc595443c95ecca059f1d0f1c04413b68b7d9
FrazierMark <60164353+FrazierMark@users.noreply.github.com>
2021-09-23 15:54:50 -0700

    Creating Checkout Product component.

--- amazon_clone_app/src/components/CheckoutProduct.js
+++ amazon_clone_app/src/components/CheckoutProduct.js
@@ -2,10 +2,28 @@ import React from 'react'
 import "./CheckoutProduct.css"
 
 
-const CheckoutProduct = () => {
+const CheckoutProduct = ({ id, image, title, price, rating }) => {
     return (
-        <div className="checkoutProduct">
-            
+        <div className='checkoutProduct'>
+            <img className='checkoutProduct__image' src={image} />
+
+            <div className='checkoutProduct__info'>
+                <p className='checkoutProduct__title'>{title}</p>
+                <p className="checkoutProduct__price">
+                    <small>$</small>
+                    <strong>{price}</strong>
+                </p>
+                <div className="checkoutProduct__rating">
+                    {Array(rating)
+                    .fill()
+                    .map((_, i) => (
+                        <p>🌟</p>
+                    ))}
+                </div>
+                {!hideButton && (
+                    <button onClick={removeFromBasket}>Remove from Basket</button>
+                )}
+            </div>
         </div>
     )
 }
