c1f1f8e9d4a8fed8a37c7357cd4d55264f01c864
FrazierMark <60164353+FrazierMark@users.noreply.github.com>
2021-09-23 15:19:25 -0700

    Getting basket from data layer using context.

--- amazon_clone_app/src/components/Product.js
+++ amazon_clone_app/src/components/Product.js
@@ -1,7 +1,25 @@
+import { ratingClasses } from '@mui/material';
 import React from 'react'
 import "./Product.css"
+import {useStateValue} from "./StateProvider"
 
-const Product = ({id, title, image, price, rating }) => {
+const Product = ({ id, title, image, price, rating }) => {
+    const [state, dispatch] = useStateValue();
+
+        // Send/dispatch item to data layer/context
+    const addToBasket = () => {
+        dispatch({
+            type: 'ADD_TO_BASKET',
+            item: {
+                id: id,
+                title: title,
+                image: image,
+                price: price,
+                rating: rating,
+            },
+        });
+    };
+
     return (
         <div className="product">
             <div className="Product__info">
@@ -21,7 +39,7 @@ const Product = ({id, title, image, price, rating }) => {
             </div>
             <img src={image} alt="" />
             
-            <button>Add to Basket</button>
+            <button onClick={addToBasket}>Add to Basket</button>
         </div>
     )
 }
