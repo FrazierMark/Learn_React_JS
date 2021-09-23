efeb305e8442a9b8da4a890db71350ac0198659f
FrazierMark <60164353+FrazierMark@users.noreply.github.com>
2021-09-22 19:37:49 -0700

    Working on creating Checkout Page.

---
+++ amazon_clone_app/src/components/Checkout.js
@@ -0,0 +1,14 @@
+import React from 'react'
+import "./Checkout.css"
+
+const Checkout = () => {
+    return (
+        <div>
+            <div className="checkout">
+                <h1>placeholder</h1>
+            </div>
+        </div>
+    )
+}
+
+export default Checkout
