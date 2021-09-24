58772129ccc0a1244cc0675e6cee9bf510fa8519
FrazierMark <60164353+FrazierMark@users.noreply.github.com>
2021-09-24 11:34:29 -0700

    Setting up auth using firebase

--- amazon_clone_app/src/firebase.js
+++ amazon_clone_app/src/firebase.js
@@ -1,3 +1,5 @@
+import firebase from "firebase";
+
 // For Firebase JS SDK v7.20.0 and later, measurementId is optional
 const firebaseConfig = {
     apiKey: "AIzaSyALOTDLc9qY4TGlgrHHMHf-tLTSnjefsk8",
@@ -7,4 +9,12 @@ const firebaseConfig = {
     messagingSenderId: "331855031792",
     appId: "1:331855031792:web:a65b74c15808e8cbd70135",
     measurementId: "G-DSD901Z89L"
-  };
+};
+  
+const firebaseApp = firebase.initializeApp(firebaseConfig);
+
+const db = firebaseApp.firestore();
+const auth = firebase.auth();
+
+
+export { db, auth };
