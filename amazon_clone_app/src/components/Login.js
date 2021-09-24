e101d9d6ae201d83effc0a2d3f58885de7e69420
FrazierMark <60164353+FrazierMark@users.noreply.github.com>
2021-09-24 11:03:33 -0700

    Created Login.css

--- amazon_clone_app/src/components/Login.js
+++ amazon_clone_app/src/components/Login.js
@@ -1,10 +1,13 @@
 import React from 'react'
 import "./Login.css"
+import { Link } from 'react-router-dom'
 
 const Login = () => {
     return (
         <div className="Login">
+            <Link to='/'>
             <img src='https://upload.wikimedia.org/wikipedia/commons/thumb/a/a9/Amazon_logo.svg/1024px-Amazon_logo.svg.png'/>
+            </Link>
         </div>
     )
 }
