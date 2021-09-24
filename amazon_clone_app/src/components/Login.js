a5444fe8c7739f3357dbe93bb7c58ea96d09425f
FrazierMark <60164353+FrazierMark@users.noreply.github.com>
2021-09-24 11:08:36 -0700

    Finishing login page.

--- amazon_clone_app/src/components/Login.js
+++ amazon_clone_app/src/components/Login.js
@@ -8,6 +8,29 @@ const Login = () => {
             <Link to='/'>
             <img src='https://upload.wikimedia.org/wikipedia/commons/thumb/a/a9/Amazon_logo.svg/1024px-Amazon_logo.svg.png'/>
             </Link>
+
+            <div className="login__container">
+                <h1>Sign-in</h1>
+
+                <form>
+                    <h5>E-mail</h5>
+                    <input type="text" />
+                    
+                    <h5>Pass</h5>
+                    <input type="password" />
+
+                    <button>Sign In</button>
+                </form>
+
+                <p>
+                    By signing-in you agree to the AMAZON FAKE CLONE Conditions of Use & Sale. Please
+                    see our Privacy Notice, our Cookies Notice and our Interest-Based Ads Notice.
+                </p>
+
+                <button className='login__registerButton'>Create your Amazon Account</button>
+
+            </div>
+
         </div>
     )
 }
