99a6a20a2ed16872495193404884cb6d145ac833
FrazierMark <60164353+FrazierMark@users.noreply.github.com>
2021-09-27 13:38:30 -0700

    Payment processing.

--- amazon_clone_app/src/components/Payment.js
+++ amazon_clone_app/src/components/Payment.js
@@ -1,15 +1,31 @@
-import React from 'react'
+import React, { useState } from 'react'
 import CheckoutProduct from './CheckoutProduct';
 import "./Payment.css";
 import { useStateValue } from './StateProvider';
 import { Link } from 'react-router-dom';
 import { CardElement, useStripe, useElements } from "@stripe/react-stripe-js";
+import { getBasketTotal } from './reducer';
+import CurrencyFormat from 'react-currency-format';
 
 const Payment = () => {
     const [{ basket, user }, dispatch] = useStateValue();
+    const [error, setError] = useState(null);
+    const [disabled, setDisabled] = useState(true);
 
     const stripe = useStripe();
     const elements = useElements();
+
+    const handleSubmit = e => {
+        //Implement stripe processing
+    };
+
+    const handleChange = (e) => {
+        //Listen for changes in CardElement
+        //and display any errors as the customer types card info
+        setDisabled(e.empty);
+        setError(e.error ? e.error.message : "");
+
+    }
 
     return (
         <div className="payment" >
@@ -55,6 +71,23 @@ const Payment = () => {
                         <h3> Payment Method</h3>
                     </div>
                     <div className="payment__details">
+
+                        <form onSubmit={handleSubmit}>
+                            <CardElement onChange={handleChange} />
+                            
+                            <div className="payment__priceContainer">
+                                <CurrencyFormat
+                                    renderText={(value) => (
+                                            <h3> Order Total: {value} </h3>
+                                    )}
+                                    decimalScale={2}
+                                    value={getBasketTotal(basket)}
+                                    displayType={"text"}
+                                    thousandSeperator={true}
+                                    prefix={"$"}
+                                />
+                            </div>
+                        </form>
 
                     </div>
                     
