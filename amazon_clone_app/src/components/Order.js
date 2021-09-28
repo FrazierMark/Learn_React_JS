83e7ea7ece8922010c686d321c15fcebc66c26dd
FrazierMark <60164353+FrazierMark@users.noreply.github.com>
2021-09-28 14:54:17 -0700

    Styling Orders

--- amazon_clone_app/src/components/Order.js
+++ amazon_clone_app/src/components/Order.js
@@ -2,6 +2,7 @@ import React from 'react'
 import "./Order.css"
 import moment from "moment"
 import CheckoutProduct from './CheckoutProduct';
+import CurrencyFormat from 'react-currency-format';
 
 const Order = ({order}) => {
     return (
@@ -21,6 +22,16 @@ const Order = ({order}) => {
                 rating={item.rating}
                 history={item.history}/>
         ))}
+            <CurrencyFormat
+                renderText={(value) => (
+                        <h3 className="order__total"> Order Total: {value} </h3>
+                )}
+                decimalScale={2}
+                value={order.data.amount / 100}
+                displayType={"text"}
+                thousandSeperator={true}
+                prefix={"$"}
+            />
         </div>
     )
 }
