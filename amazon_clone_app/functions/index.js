056ed6455e1ab6ea93da0c45d3758881bd06413b
FrazierMark <60164353+FrazierMark@users.noreply.github.com>
2021-09-28 13:06:20 -0700

    Setting up API for cloud function backend.

--- amazon_clone_app/functions/index.js
+++ amazon_clone_app/functions/index.js
@@ -1,9 +1,22 @@
 const functions = require("firebase-functions");
+const express = require("express");
+const cors = require("cors");
+const stripe = require("stripe")('sk_test_51JeQ98Lu7BrQj7NTf0wBQTOrmsANqExLQlmjy7oIxhqN57CEspmiEChkBeBswHL9zDwqkgQK51GJAQVLv4kq2o0A00mqvHAwap')
 
-// // Create and Deploy Your First Cloud Functions
-// // https://firebase.google.com/docs/functions/write-firebase-functions
-//
-// exports.helloWorld = functions.https.onRequest((request, response) => {
-//   functions.logger.info("Hello logs!", {structuredData: true});
-//   response.send("Hello from Firebase!");
-// });
+// - API
+
+
+// - App config
+const app = express();
+
+
+// - Middlewares
+app.use(cors({ origin: true }));
+app.use(express.json());
+
+// - API routes
+app.get('/', (request, response) => response.status(200).send ('hello world'))
+
+
+// - Listen command
+exports.api = functions.https.onRequest(app)
