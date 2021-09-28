ad4a2900684e22bbf20b68e9ca1365b7ad3819b3
FrazierMark <60164353+FrazierMark@users.noreply.github.com>
2021-09-28 15:50:05 -0700

    Final deploy

--- amazon_clone_app/src/components/axios.js
+++ amazon_clone_app/src/components/axios.js
@@ -1,6 +1,9 @@
 import axios from "axios";
 
 const instance = axios.create({
-    baseURL: 'http://localhost:5001/clone-app-da065/us-central1/api' // API (cloud function) URL
+    baseURL: 'https://us-central1-clone-app-da065.cloudfunctions.net/api' // API (cloud function) URL
 });
 export default instance;
+
+
+// http://localhost:5001/clone-app-da065/us-central1/api
