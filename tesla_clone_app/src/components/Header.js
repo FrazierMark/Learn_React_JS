b64f94722cf6965e64f3d36ee7d5eb5b4b7aba72
FrazierMark <60164353+FrazierMark@users.noreply.github.com>
2021-09-29 10:57:00 -0700

    Working on BurgerMenu

--- tesla_clone_app/src/components/Header.js
+++ tesla_clone_app/src/components/Header.js
@@ -1,6 +1,8 @@
 import React from 'react';
 import styled from "styled-components"
 import MenuOpenIcon from '@mui/icons-material/MenuOpen';
+import CloseIcon from '@mui/icons-material/Close';
+
 
 const Header = () => {
     return (
@@ -16,9 +18,21 @@ const Header = () => {
             </Menu>
             <RightMenu>
                 <a href="#">Shop</a>
-                <a href="#">Tesla Account</a>
+                <a href="#">Tesla  Account</a>
                 <CustomMenu />
             </RightMenu>
+            <BurgerNav>
+                <CloseWrapper>
+                    <CloseMenu />
+                </CloseWrapper>
+                <li><a href="#">Existing Inventory</a></li>
+                <li><a href="#">Used Inventory</a></li>
+                <li><a href="#">Trade-In</a></li>
+                <li><a href="#">Cybertruck</a></li>
+                <li><a href="#">Roadster</a></li>
+                <li><a href="#">Existing Inventory</a></li>
+            </BurgerNav>
+
         </Container>
     )
 }
@@ -28,10 +42,12 @@ export default Header
 
 
 const Container = styled.div`
+    z-index: 1;
     min-height: 60px;
     position: fixed;
     display: flex;
     align-items: center;
+    justify-content: space-between;
     padding: 0 20px;
     top: 0;
     left: 0;
@@ -52,6 +68,10 @@ const Menu = styled.div`
         padding: 0 10px;
         flex-wrap: no-wrap;
     }
+
+    @media(max-width: 768) {
+        display: none;
+    }
 `
 
 const RightMenu = styled.div`
@@ -69,4 +89,34 @@ const RightMenu = styled.div`
 const CustomMenu = styled(MenuOpenIcon)`
     cursor: pointer;
 
+`
+const BurgerNav = styled.div`
+    position: fixed;
+    top: 0;
+    bottom: 0;
+    right: 0;
+    background: white;
+    width: 300px;
+    z-index: 16;
+    list-style: none;
+    padding: 20px;
+    display: flex;
+    flex-direction: column;
+    text-align: start;
+    li {
+        padding: 15px 0;
+        border-bottom: 1px solid rgba(0, 0, 0, .2);
+
+        a {
+            font-weight: 600;
+        }
+    }
+`
+
+const CloseMenu = styled(CloseIcon)`
+
+    `
+const CloseWrapper = styled.div`
+    display: flex;
+    justify-content: flex-end;
 `
--- tesla_clone_app/src/components/Section.js
+++ tesla_clone_app/src/components/Section.js
@@ -1,15 +1,20 @@
 import React from 'react'
 import styled from 'styled-components'
+import Fade from 'react-reveal/Fade'
 
 const Section = ({title, description, backgroundImg, leftBtnText, rightBtnText}) => {
     return (
         <Wrap bgImage={backgroundImg}>
+            <Fade bottom>
             <ItemText>
                 <h1>{title}</h1>
                 <p>{description}</p>
             </ItemText>
+            </Fade>
 
             <Buttons>
+
+            <Fade bottom>
             <ButtonGroup>
                 <LeftButton>
                     {leftBtnText}
@@ -20,8 +25,10 @@ const Section = ({title, description, backgroundImg, leftBtnText, rightBtnText})
                         <RightButton>
                         {rightBtnText}
                         </RightButton> }
-                
+
             </ButtonGroup>
+                </Fade>
+                
             <DownArrow src="/images/down-arrow.svg" />
             </Buttons>
 
