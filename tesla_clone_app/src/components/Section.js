7120d9cf62a3db5ac06a55797ee1cb471d581fc6
FrazierMark <60164353+FrazierMark@users.noreply.github.com>
2021-09-28 18:27:00 -0700

    Working on main home page background.

---
+++ tesla_clone_app/src/components/Section.js
@@ -0,0 +1,22 @@
+import React from 'react'
+import styled from 'styled-components'
+
+const Section = () => {
+    return (
+        <Wrap>
+            
+        </Wrap>
+    )
+}
+
+export default Section
+
+
+const Wrap = styled.div`
+    width: 100vw;
+    height: 100vh;
+    background-size: cover;
+    background-position: center;
+    backjground-repeat: not-repeat;
+    background-image: url('/images/model-s.jpg');
+`
